3c0f21873cfca7ce7f8bd30b8370933ee06abc0f
Krunal Kevadiya <kevadiyakrunal22@gmail.com>
2023-10-28 09:56:07 +0530

    release: v0.0.3 version

--- krupy/__init__.py
+++ krupy/__init__.py
@@ -5,5 +5,4 @@ Docs: https://krupy.readthedocs.io/
 
 from .main import *  # noqa: F401,F403
 
-# This version is a placeholder autoupdated by poetry-dynamic-versioning
-__version__ = "0.0.0"
+__version__ = "0.0.3"
